1d6dde52b358d720937c6cd908c5ea12ca3af445
CrispyHarder <christian.harder96@gmail.com>
2021-12-10 14:43:24 +0100

    .

--- models/resnet.py
+++ models/resnet.py
@@ -38,7 +38,7 @@ def _weights_init(m):
     if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
         init.kaiming_normal_(m.weight)
 
-class LambdaLayer(nn.Module):
+class LambdaLayer(nn.Module): 
     def __init__(self, lambd):
         super(LambdaLayer, self).__init__()
         self.lambd = lambd
