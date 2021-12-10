0bea0a3e1c3694caa6ae1cdd12051480eec0c2c1
CrispyHarder <christian.harder96@gmail.com>
2021-12-10 14:53:24 +0100

    .

--- train_ghn_self.py
+++ train_ghn_self.py
@@ -68,7 +68,7 @@ if __name__ == '__main__':
 
     #get the resnets with their graphs
     res20, res32, res44 = resnet20(), resnet32(), resnet44()
-    models = [res20, res32, res44]
+    models = [res20]
 
     graphs = GraphBatch([Graph(model, ve_cutoff=50) for model in models])
     graphs.to_device(device)
