89110333b77b646cfe72f4d6a9d099b92807ea00
CrispyHarder <christian.harder96@gmail.com>
2021-12-21 16:18:50 +0100

    added file and code to train resnet 20

--- _dwp/utils.py
+++ _dwp/utils.py
@@ -18,6 +18,38 @@ import yaml
 import pickle
 DATA_ROOT = os.path.join('data')# os.environ['DATA_ROOT']
 
+
+def load_ghn_noise(path, device=None):
+
+    from ppuda.ghn.nn import GHN2
+    from ppuda.ghn.decoder import ConvDecoder
+
+    with open(os.path.join(path, 'params.yaml')) as f:
+        args = yaml.full_load(f)
+
+    ghn = GHN2('cifar10')
+    hid = ghn.hid
+    var_init = args['var_init']
+    mu_scale = args['mu_scale']
+    var_scale = args['var_scale']
+    train_noise = args['train_noise']
+    
+    ghn.decoder = ConvDecoder(in_features=hid,
+                        hid=(hid * 4, hid * 8),
+                        out_shape=(64,64,3,3),
+                        num_classes=10,
+                        gen_noise = True,
+                        var_init = var_init,
+                        mu_scale  = mu_scale,
+                        var_scale = var_scale,
+                        train_noise= train_noise)
+
+    if device:
+        ghn.load_state_dict(torch.load(os.path.join(path, 'ghn_params.torch'),map_location=device))
+    else:
+        ghn.load_state_dict(torch.load(os.path.join(path, 'ghn_params.torch')))
+    return ghn
+
 class ConvDataset(Dataset):
     def __init__(self, file=None, data=None):
         super(ConvDataset, self).__init__()
