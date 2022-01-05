4a13a4a0478269c046c21a05cd39d17b959a1dbe
CrispyHarder <christian.harder96@gmail.com>
2022-01-05 13:36:20 +0100

    deleted a lot of unused stuff in train net

--- train-net.py
+++ train-net.py
@@ -1,12 +1,10 @@
 import torch
-from torch import nn
 import _dwp.utils as utils
 import numpy as np
 import os
 import time
 from models.resnet import ResNet
 from _dwp.logger import Logger
-from torch.optim.lr_scheduler import MultiStepLR
 import _dwp.myexman as myexman
 from torch.nn import functional as F
 from torch.utils.tensorboard import SummaryWriter
@@ -54,16 +52,7 @@ parser.add_argument('--hid_dim', default=[32, 64], type=int, nargs='+')
 parser.add_argument('--n_classes', default=10, type=int)
 parser.add_argument('--do', default=[], type=float, nargs='*')
 
-#model init settings SINGLE init
-parser.add_argument('--pretrained', default='')
-parser.add_argument('--filters_list', default=[], nargs='*', type=str)
-parser.add_argument('--init', default='xavier')
-parser.add_argument('--init_list', type=str, nargs='*', default=[])
-parser.add_argument('--vae', default='')
-parser.add_argument('--vae_list', type=str, nargs='*', default=[])
-
 #model init settings MULTI init (if used, single init is ignored)
-parser.add_argument('--mult_init', default= 1, type = int)
 parser.add_argument('--mult_init_mode', default= 'xavier', type = str,
                     help = '''such as vqvae1.3''')
 parser.add_argument('--mult_init_root', type=str, default=os.path.join('data','resnet20','3x3'))
@@ -109,51 +98,18 @@ logger = Logger('logs', base=args.root, fmt=fmt)
 trainloader, testloader = utils.load_dataset(data=args.data, train_bs=args.bs, test_bs=args.test_bs,
                                              num_examples=args.num_examples, seed=args.data_split_seed,
                                              augmentation=(args.aug == 1))
-                                             
-if args.model == 'resnet20':
-    net = ResNet([3,3,3],num_classes=args.n_classes)
-else:
-    raise NotImplementedError
-
-# Initialization
-if args.mult_init == 1 : 
-    net.mult_weights_init(args.mult_init_mode, args.mult_init_root, device=device, prior=args.mult_init_prior)
-else:
-    if hasattr(net, 'weights_init'):
-        net.weights_init(args.init_list, args.vae_list, pretrained=args.pretrained, filters_list=args.filters_list)
-    else:
-        utils.net_init(net, args.init, args.vae)
-
-if args.dwp_reg != 0:
-    net.set_dwp_regularizer(args.vae_list)
-
+                                            
+# Network
+net = ResNet([3,3,3],num_classes=args.n_classes)
+net.mult_weights_init(args.mult_init_mode, args.mult_init_root, device=device, prior=args.mult_init_prior)
 net = net.to(device)
-# Optimizer
-train_params = []
-if args.rfe == 0:
-    train_params = net.parameters()
-elif args.rfe == 1:
-    tr_modeules = [net.classifier]
-    train_params = list(net.classifier.parameters())
-    for m in net.features.modules():
-        if isinstance(m, nn.BatchNorm2d):
-            train_params += list(m.parameters())
-            tr_modeules += [m]
-    print('==> Random Feature Extration mode')
-    print(*tr_modeules)
-else:
-    raise NotImplementedError
 
+# Optimizer
 opt = torch.optim.SGD(net.parameters(), args.lr,
                                     momentum=args.momentum,
                                     weight_decay=args.weight_decay)
 lrscheduler = MultistepMultiGammaLR(opt, milestones=args.milestones, 
                                     gamma=args.gammas)
-
-# Load params if fine-tuning
-if args.resume:
-    net.load_state_dict(torch.load(os.path.join(args.resume, 'model.torch')))
-    opt.load_state_dict(torch.load(os.path.join(args.resume, 'opt.torch')))
 
 N = len(trainloader.dataset)
 t0 = time.time()
@@ -183,53 +139,17 @@ for e in range(1, args.epochs + 1):
         p = net(x)
 
         data_term = F.cross_entropy(p, y)
-        l2_norm = torch.FloatTensor([0.]).to(device)
-        if args.l2 != 0:
-            l2_norm = torch.sum(torch.stack([torch.sum(p**2) for p in net.features.parameters()]))
 
-        dwp_reg = 0.
-        if args.dwp_reg != 0.:
-            dwp_reg = net.get_dwp_reg(backward=True, n_tries=args.dwp_samples, weight=args.dwp_reg)
-
-        loss = data_term + args.l2 * l2_norm
+        loss = data_term 
 
         loss.backward()
 
         opt.step()
 
-        loss += args.dwp_reg * dwp_reg
-
         acc = torch.sum(p.max(1)[1] == y)
         train_acc.add(acc.item(), p.size(0))
         train_nll.add(data_term.item() * x.size(0), x.size(0))
         train_loss.add(loss.item() * x.size(0), x.size(0))
-
-        if args.fastconv == 1:
-            if (it % args.eval_freq) == 0 or it == 1:
-                net.eval()
-
-                logp_test, labels = predict(testloader, net)
-                test_acc = np.mean(logp_test.argmax(1) == labels)
-                test_nll = -logp_test[np.arange(len(labels)), labels].mean()
-
-                logger.add_scalar(it, 'loss', train_loss.get_val())
-                logger.add_scalar(it, 'train_nll', train_nll.get_val())
-                logger.add_scalar(it, 'test_nll', test_nll)
-                logger.add_scalar(it, 'train_acc', train_acc.get_val())
-                logger.add_scalar(it, 'test_acc', test_acc)
-                logger.add_scalar(it, 'lr', opt.param_groups[0]['lr'])
-                logger.add_scalar(it, 'sec', time.time() - t0)
-                logger.add_scalar(it, 'l2_norm', l2_norm.item())
-
-                logger.iter_info()
-                logger.save()
-
-                torch.save(net.state_dict(), os.path.join(args.root, 'model.torch'))
-                torch.save(opt.state_dict(), os.path.join(args.root, 'opt.torch'))
-
-                t0 = time.time()
-
-                net.train()
 
     if ((e % args.eval_freq) == 0 or e == 1) and (args.fastconv == 0):
         net.eval()
@@ -245,8 +165,6 @@ for e in range(1, args.epochs + 1):
         logger.add_scalar(e, 'test_acc', test_acc)
         logger.add_scalar(e, 'lr', opt.param_groups[0]['lr'])
         logger.add_scalar(e, 'sec', time.time() - t0)
-        logger.add_scalar(e, 'l2_norm', l2_norm.item())
-        logger.add_scalar(e, 'dwp_reg', dwp_reg)
 
         logger.iter_info()
         logger.save()
@@ -258,8 +176,6 @@ for e in range(1, args.epochs + 1):
         writer.add_scalar( 'test/acc', test_acc,e)
         writer.add_scalar( 'lr', opt.param_groups[0]['lr'],e)
         writer.add_scalar( 'sec', time.time() - t0,e)
-        writer.add_scalar( 'l2_norm', l2_norm.item(),e)
-        writer.add_scalar( 'dwp_reg', dwp_reg,e)
         
         epoch = e 
         if (epoch-1) % 10 == 0:
@@ -274,7 +190,7 @@ for e in range(1, args.epochs + 1):
    
         t0 = time.time()
 
-torch.save(net.state_dict(), os.path.join(args.root, 'vae_params_lastepoch.torch'))
+torch.save(net.state_dict(), os.path.join(args.root, 'net_params_lastepoch.torch'))
 torch.save(opt.state_dict(), os.path.join(args.root, 'opt_params_lastepoch.torch'))
 
 writer.flush()
