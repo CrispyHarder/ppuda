5978fb47692bdedfbe258c9ac0ccac88c25aaec3
CrispyHarder <christian.harder96@gmail.com>
2022-04-27 16:30:25 +0200

    modiefied ghn_ens_pcam for iters instead of epochs

--- train_ens_ghn_pcam.py
+++ train_ens_ghn_pcam.py
@@ -1,4 +1,5 @@
 import os
+from termios import TAB0
 import numpy as np
 import torch
 from torch.nn import functional as F
@@ -12,10 +13,116 @@ import time
 from torch.utils.tensorboard import SummaryWriter
 from torch.optim.lr_scheduler import MultiStepLR
 
+def predict(data,ghn):
+    val_loss = utils.AvgrageMeter()
+    val_ce = utils.AvgrageMeter()
+    val_cossim = utils.AvgrageMeter()
+    val_top1 = utils.AvgrageMeter()
+    val_res_cossim = utils.AvgrageMeter()
+    val_res_top1 = utils.AvgrageMeter()
+
+    for i,(images,labels) in enumerate(data):
+        ce_loss = 0
+        logits = 0
+        loss = 0
+        count = 0
+
+        images = images.to(device)
+        labels = labels.to(device)
+
+        #for validation models 
+        for j in range(2):
+            models_pred = ghn(models,graphs)
+            logit_vec = []
+            for i,model in enumerate(models_pred):
+                y = model(images)
+                logit_vec.append(y.flatten())
+                ce_loss += F.cross_entropy(y, labels)
+                logits += y 
+                count += 1 
+            if j == 0:
+                logits_0 = torch.cat(logit_vec).to(device)
+            if j == 1:
+                logits_1 = torch.cat(logit_vec).to(device)
+                cos_sim = CosineSimilarity(logits_0,logits_1)
+        ce_loss = ce_loss/count 
+        logits = logits/count
+        loss = ce_loss + cos_sim * csim_weight
+
+        prec1, _ = accuracy(logits, labels, topk=(1, 5))
+        n = len(labels)
+        val_loss.update(loss.item(),n)
+        val_top1.update(prec1.item(),n)
+        val_ce.update(ce_loss.item(),n)
+        val_cossim.update(cos_sim.item(),n)
+
+        # for res20 
+        for j in range(2):
+            res20 = ghn([res20],res20_graph)[0]  
+            y = res20(images)
+            if j == 0:
+                logits = y 
+            if j == 0:
+                logits_0 = y.flatten().to(device)
+            if j == 1:
+                logits_1 = y.flatten().to(device)
+                cos_sim = CosineSimilarity(logits_0,logits_1)
+
+        prec1, _ = accuracy(logits, labels, topk=(1, 5))
+        n = len(labels)
+        val_res_top1.update(prec1.item(),n)
+        val_res_cossim.update(cos_sim.item(),n)
+
+    return val_loss.avg, val_ce.avg, val_cossim.avg, val_top1.avg, val_res_cossim.avg, val_res_top1
+        
+
+def eval_step(it,net,data,logger,writer,args,opt,train_loss,train_ce,
+    train_cossim,train_top1,lrscheduler,step=True):
+    global t0 
+    global best_loss
+    
+    with torch.no_grad():
+        val_loss, val_ce, val_cossim, val_top1, val_res_cossim, val_res_top1 = predict(data,net)
+    
+    logger.add_scalar(it, 'time', time.time()-t0)
+    logger.add_scalar(it, 'train_loss', train_loss.avg)
+    logger.add_scalar(it, 'train_ce', train_ce.avg)
+    logger.add_scalar(it, 'train_cossim', train_cossim.avg)
+    logger.add_scalar(it, 'train_top1', train_top1.avg)
+    logger.add_scalar(it, 'val_loss', val_loss)
+    logger.add_scalar(it, 'val_ce', val_ce)
+    logger.add_scalar(it, 'val_cossim', val_cossim)
+    logger.add_scalar(it, 'val_top1', val_top1)
+    logger.add_scalar(it, 'res_top1', val_res_top1)
+    logger.add_scalar(it, 'two_res_cossim', val_res_cossim)
+    logger.iter_info()
+    logger.save()
+
+    writer.add_scalar('time', time.time()-t0,it)
+    writer.add_scalar('train/loss', train_loss.avg,it)
+    writer.add_scalar('train/ce', train_ce.avg,it)
+    writer.add_scalar('train/cossim', train_cossim.avg,it)
+    writer.add_scalar('train/top1', train_top1.avg,it)
+    writer.add_scalar('val/loss', val_loss,it)
+    writer.add_scalar('val/ce', val_ce,it)
+    writer.add_scalar('val/cossim', val_cossim,it)
+    writer.add_scalar('val/top1', val_top1,it)
+    writer.add_scalar('val/res_top1', val_res_top1,it)
+    writer.add_scalar('val/two_res_cossim', val_res_cossim,it)
+
+    is_best = best_loss > val_loss
+    if is_best:
+        best_loss = val_loss
+        torch.save(net.state_dict(), os.path.join(args.root, 'ghn_params_val.torch')) 
+
+    t0 = time.time()
+    if step:
+        scheduler.step()
+
 if __name__ == '__main__':
     parser = myexman.ExParser(file=__file__)
     #train settings 
-    parser.add_argument('--num_epochs', default=60, type=int)
+    parser.add_argument('--num_epochs', default=50, type=int)
     parser.add_argument('--batch_size', default=64, type=int)
     parser.add_argument('--seed', default=42, type=int)
     parser.add_argument('--data_split_seed', default=42, type=int)
@@ -96,100 +203,23 @@ if __name__ == '__main__':
     writer = SummaryWriter(args.root)
     best_loss = 11e8
 
+    it = 0
+    t0 = time.time()
+
     # a first eval before training 
-    val_loss = utils.AvgrageMeter()
-    val_ce = utils.AvgrageMeter()
-    val_cossim = utils.AvgrageMeter()
-    val_top1 = utils.AvgrageMeter()
-    val_res_cossim = utils.AvgrageMeter()
-    val_res_top1 = utils.AvgrageMeter()
+    train_loss = utils.AvgrageMeter()
+    train_ce = utils.AvgrageMeter()
+    train_cossim = utils.AvgrageMeter()
+    train_top1 = utils.AvgrageMeter()
+    eval_step(it,ghn,valloader,logger,writer,args,optimizer,train_loss,
+        train_ce,train_cossim,train_top1,scheduler,step=False)
+    torch.save(ghn.state_dict(), os.path.join(args.root, 'ghn_params_init.torch'))
 
-    for i,(images,labels) in enumerate(valloader):
-        ce_loss = 0
-        logits = 0
-        loss = 0
-        count = 0
-
-        images = images.to(device)
-        labels = labels.to(device)
-
-        #for validation models 
-        for j in range(2):
-            models_pred = ghn(models,graphs)
-            logit_vec = []
-            for i,model in enumerate(models_pred):
-                y = model(images)
-                logit_vec.append(y.flatten())
-                ce_loss += F.cross_entropy(y, labels)
-                logits += y 
-                count += 1 
-            if j == 0:
-                logits_0 = torch.cat(logit_vec).to(device)
-            if j == 1:
-                logits_1 = torch.cat(logit_vec).to(device)
-                cos_sim = CosineSimilarity(logits_0,logits_1)
-        ce_loss = ce_loss/count 
-        logits = logits/count
-        loss = ce_loss + cos_sim * csim_weight
-
-        prec1, _ = accuracy(logits, labels, topk=(1, 5))
-        n = len(labels)
-        val_loss.update(loss.item(),n)
-        val_top1.update(prec1.item(),n)
-        val_ce.update(ce_loss.item(),n)
-        val_cossim.update(cos_sim.item(),n)
-
-        # for res20 
-        for j in range(2):
-            res20 = ghn([res20],res20_graph)[0]  
-            y = res20(images)
-            if j == 0:
-                logits = y 
-            if j == 0:
-                logits_0 = y.flatten().to(device)
-            if j == 1:
-                logits_1 = y.flatten().to(device)
-                cos_sim = CosineSimilarity(logits_0,logits_1)
-        
-        prec1, _ = accuracy(logits, labels, topk=(1, 5))
-        n = len(labels)
-        val_res_top1.update(prec1.item(),n)
-        val_res_cossim.update(cos_sim.item(),n)
-
-        logger.add_scalar(0, 'val_loss', val_loss.avg)
-        logger.add_scalar(0, 'val_ce', val_ce.avg)
-        logger.add_scalar(0, 'val_cossim', val_cossim.avg)
-        logger.add_scalar(0, 'val_top1', val_top1.avg)
-        logger.add_scalar(0, 'res_top1', val_res_top1.avg)
-        logger.add_scalar(0, 'two_res_cossim', val_res_cossim.avg)
-        logger.iter_info()
-        logger.save()
-        writer.add_scalar('val/loss',val_loss.avg,0)
-        writer.add_scalar('val/ce', val_ce.avg,0)
-        writer.add_scalar('val/cossim', val_cossim.avg,0)
-        writer.add_scalar('val/top1', val_top1.avg,0)
-        writer.add_scalar('val/res_top1', val_res_top1.avg,0)
-        writer.add_scalar('val/two_res_cossim', val_res_cossim.avg,0)
-
-        torch.save(ghn.state_dict(), os.path.join(args.root, 'ghn_params_init.torch'))
-
+    #training
     for epoch in range(1, args.num_epochs + 1):
-        
-        train_loss = utils.AvgrageMeter()
-        train_ce = utils.AvgrageMeter()
-        train_cossim = utils.AvgrageMeter()
-        train_top1 = utils.AvgrageMeter()
-
-        val_loss = utils.AvgrageMeter()
-        val_ce = utils.AvgrageMeter()
-        val_cossim = utils.AvgrageMeter()
-        val_top1 = utils.AvgrageMeter()
-        val_res_cossim = utils.AvgrageMeter()
-        val_res_top1 = utils.AvgrageMeter()
-
-        start_epoch = time.time()
 
         for i,(images,labels) in enumerate(trainloader):
+            it += 1 
             ce_loss = 0
             logits = 0
             loss = 0
@@ -234,96 +264,19 @@ if __name__ == '__main__':
             train_ce.update(ce_loss.item(),n)
             train_cossim.update(cos_sim.item(),n)
 
-        for i,(images,labels) in enumerate(valloader):
-            ce_loss = 0
-            logits = 0
-            loss = 0
-            count = 0
-
-            images = images.to(device)
-            labels = labels.to(device)
-
-            #for validation models 
-            for j in range(2):
-                models_pred = ghn(models,graphs)
-                logit_vec = []
-                for i,model in enumerate(models_pred):
-                    y = model(images)
-                    logit_vec.append(y.flatten())
-                    ce_loss += F.cross_entropy(y, labels)
-                    logits += y 
-                    count += 1 
-                if j == 0:
-                    logits_0 = torch.cat(logit_vec).to(device)
-                if j == 1:
-                    logits_1 = torch.cat(logit_vec).to(device)
-                    cos_sim = CosineSimilarity(logits_0,logits_1)
-            ce_loss = ce_loss/count 
-            logits = logits/count
-            loss = ce_loss + cos_sim * csim_weight
-
-            prec1, _ = accuracy(logits, labels, topk=(1, 5))
-            n = len(labels)
-            val_loss.update(loss.item(),n)
-            val_top1.update(prec1.item(),n)
-            val_ce.update(ce_loss.item(),n)
-            val_cossim.update(cos_sim.item(),n)
+            if it % 300 == 0: 
+                eval_step(it,ghn,valloader,logger,writer,args,optimizer,train_loss,
+                        train_ce,train_cossim,train_top1,scheduler,step=True)
+                train_loss = utils.AvgrageMeter()
+                train_ce = utils.AvgrageMeter()
+                train_cossim = utils.AvgrageMeter()
+                train_top1 = utils.AvgrageMeter()
 
 
-            # for res20 
-            for j in range(2):
-                res20 = ghn([res20],res20_graph)[0]  
-                y = res20(images)
-                if j == 0:
-                    logits = y 
-                if j == 0:
-                    logits_0 = y.flatten().to(device)
-                if j == 1:
-                    logits_1 = y.flatten().to(device)
-                    cos_sim = CosineSimilarity(logits_0,logits_1)
-            
-            prec1, _ = accuracy(logits, labels, topk=(1, 5))
-            n = len(labels)
-            val_res_top1.update(prec1.item(),n)
-            val_res_cossim.update(cos_sim.item(),n)
+        torch.save(ghn.state_dict() , os.path.join(args.root, 'ghn_params_epoch_{}.torch'.format(epoch)))
+        torch.save(optimizer.state_dict(), os.path.join(args.root, 'opt_params_epoch_{}.torch'.format(epoch)))
 
-        duration = time.time()-start_epoch   
-        logger.add_scalar(epoch, 'time', duration)
-        logger.add_scalar(epoch, 'train_loss', train_loss.avg)
-        logger.add_scalar(epoch, 'train_ce', train_ce.avg)
-        logger.add_scalar(epoch, 'train_cossim', train_cossim.avg)
-        logger.add_scalar(epoch, 'train_top1', train_top1.avg)
-        logger.add_scalar(epoch, 'val_loss', val_loss.avg)
-        logger.add_scalar(epoch, 'val_ce', val_ce.avg)
-        logger.add_scalar(epoch, 'val_cossim', val_cossim.avg)
-        logger.add_scalar(epoch, 'val_top1', val_top1.avg)
-        logger.add_scalar(epoch, 'res_top1', val_res_top1.avg)
-        logger.add_scalar(epoch, 'two_res_cossim', val_res_cossim.avg)
-        logger.iter_info()
-        logger.save()
-
-        writer.add_scalar('time', duration,epoch)
-        writer.add_scalar('train/loss', train_loss.avg,epoch)
-        writer.add_scalar('train/ce', train_ce.avg,epoch)
-        writer.add_scalar('train/cossim', train_cossim.avg,epoch)
-        writer.add_scalar('train/top1', train_top1.avg,epoch)
-        writer.add_scalar('val/loss', val_loss.avg,epoch)
-        writer.add_scalar('val/ce', val_ce.avg,epoch)
-        writer.add_scalar('val/cossim', val_cossim.avg,epoch)
-        writer.add_scalar('val/top1', val_top1.avg,epoch)
-        writer.add_scalar('val/res_top1', val_res_top1.avg,epoch)
-        writer.add_scalar('val/two_res_cossim', val_res_cossim.avg,epoch)
-
-        if (epoch-1) % 10 == 0:
-            torch.save(ghn.state_dict() , os.path.join(args.root, 'ghn_params_epoch_{}.torch'.format(epoch)))
-            torch.save(optimizer.state_dict(), os.path.join(args.root, 'opt_params_epoch_{}.torch'.format(epoch)))
-
-        is_best = (val_loss.avg < best_loss)
-        if is_best:
-            best_loss = val_loss.avg
-            torch.save(ghn.state_dict(), os.path.join(args.root, 'ghn_params_val.torch')) 
-
-        scheduler.step()
+        
 
     torch.save(ghn.state_dict(), os.path.join(args.root, 'ghn_params_lastepoch.torch'))
     torch.save(optimizer.state_dict(), os.path.join(args.root, 'opt_params_lastepoch.torch'))
